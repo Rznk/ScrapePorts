312ba283622e31aebeec1b5f0341459572d535b3
Andrew Reznik <rezyadlf@gmail.com>
2017-08-25 01:31:56 +0300

    First commit

---
+++ grab.js
@@ -0,0 +1,35 @@
+let request = require('request');
+let cheerio = require('cheerio');
+let fs = require('fs');
+let res;
+
+let requestPorts = async ()=>{
+    for(let y = 1; y< 386; y++) {
+        await request("https://www.marinetraffic.com/ua/ais/index/ports/all/page:"+ y +"/per_page:50", async (err, response, body) => {
+            if (!err) {
+                let $ = cheerio.load(body, {
+                    normalizeWhitespace: true
+                });
+                for(let i = 2; i < 52; i ++) {
+                    let a = $("body > main > div > div > div:nth-child(1) > div:nth-child(5) > div.filters_results > div:nth-child(2) > div > div.mt-table.mt-table-responsive > table > tbody > tr:nth-child(" + i + ") > td:nth-child(2) > a");
+                    try {
+                        res = unescape(a[0].attribs.href);
+                        let idPort = res.split("/")[5];
+                        let Country = (res.split("/")[6]).split("_")[0];
+                        let port = (res.split("/")[6]).split(":")[1];
+                        let result = idPort + "/" + Country + "/" + port + "\n";
+                        console.log(result);
+                        fs.appendFileSync("ports.txt", result)
+                    } catch (e) {
+                        if(e){
+                            fs.appendFileSync("errors.txt", res + "\n")
+                        }
+                    }
+                }
+            }
+        });
+    }
+};
+
+
+requestPorts()
